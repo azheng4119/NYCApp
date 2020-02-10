78e2696ade9418d33a4c9aa7fca1f3045233562f
azheng4119 <azheng4119@bths.edu>
2020-02-10 15:58:07 -0500

    gitignore update

---
+++ NYCApp/NYCApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NYCApp
+//
+//  Created by Andy Zheng on 2/10/20.
+//  Copyright © 2020 Andy Zheng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
